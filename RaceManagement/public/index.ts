1724cfe6bf3ce1fee8f47eb811226f0d5cf69072
SaydamArda <91024118+SaydamArda@users.noreply.github.com>
2024-05-20 15:11:28 +0200

    feat: carService works, fetching doesnt work for now

--- RaceManagement/public/index.ts
+++ RaceManagement/public/index.ts
@@ -1,4 +1,41 @@
-document.addEventListener('DOMContentLoaded', (event) => {
+import { fetchRestEndpoint } from "./css/fetchRestEndpoint";
+import {Car} from "../src/model/car-model"
+
+
+async function fetchCars(): Promise<Car[]>{
+    try{
+        return await fetchRestEndpoint('/api/cars/all', 'GET');
+    }catch (e) {
+        console.error(e);
+        throw e;
+    }
+}
+
+function fillList(cars: Car[]): void{
+    const list = document.getElementById('myList2');
+    if(list){
+        cars.forEach(car => {
+            list.innerHTML = ''; // Clear existing items
+            const li = document.createElement('li');
+            li.textContent = car.carName;
+            li.id = car.carId.toString();
+            li.draggable = true;
+            list.appendChild(li);
+        });
+    }
+}
+
+
+document.addEventListener('DOMContentLoaded', async () => {
+
+    try{
+        const cars = await fetchCars();
+        fillList(cars);
+    }catch (e) {
+        console.error(e);
+    }
+
+
     const dragStart = (event: DragEvent) => {
         if (event.target instanceof HTMLElement) {
             event.dataTransfer?.setData('text/plain', event.target.id);
@@ -44,3 +81,6 @@ document.addEventListener('DOMContentLoaded', (event) => {
         list.addEventListener('drop', drop);
     });
 });
+
+
+
